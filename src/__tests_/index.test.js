d2b71cee0ea323570ccb95844ecc6914e8b9a1b8
Romello Goodman <romellogood@gmail.com>
2019-11-23 19:59:33 -0500

    Initial commit

---
+++ src/__tests_/index.test.js
@@ -0,0 +1,7 @@
+const propUp = require('../index');
+
+describe("test", () => {
+  it("should test", () => {
+    expect(propUp()).toEqual(true);
+  });
+});
---
+++ src/index.js
@@ -0,0 +1,7 @@
+const propUp = () => {
+  console.log("propUp propUp propUp");
+
+  return true;
+};
+
+module.exports = propUp;
